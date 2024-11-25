9b43a3ffbba439b67ff4e0df0e0cf77b1eb7c057
Nergis <me@nergis.dev>
2024-11-25 22:53:40 +0900

    Changesasdasdasd

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import *
 from pycomcigan import TimeTable
+from starlette.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
