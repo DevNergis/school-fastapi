a80a4be0bc02584b55bb011a26124a568211cbcc
Nergis <me@nergis.dev>
2024-12-19 22:41:44 +0900

    Add poetry.lock file for dependency management
    This commit introduces the `poetry.lock` file generated by Poetry. It ensures consistent dependency versions across environments, improving reproducibility and stability. Do not manually edit this file; it is managed automatically by Poetry.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,13 +17,13 @@ app.add_middleware(
 
 @app.get("/")
 async def main(grade: int, classnum: int):
-    td = {'Monday': '', 'Tuesday': '', 'Wednesday': '', 'Thursday': '', 'Friday': ''}
-    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+    td = {"Monday": "", "Tuesday": "", "Wednesday": "", "Thursday": "", "Friday": ""}
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
     # 시간표 가져오기
     # week_num: 0이면 이번주, 1이면 다음주
     timetable = TimeTable("근명고등학교", week_num=0)
 
     for i in range(0, 5):
-        td[days[i]] = timetable.timetable[grade][classnum][i+1]
+        td[days[i]] = timetable.timetable[grade][classnum][i + 1]
 
     return td
